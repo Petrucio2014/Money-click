3cf1e3a5c5d9c269a1b5f2fedcf2487fafe17804
Petrucio2014 <petrucio20111@hotmail.com>
2019-03-10 10:04:20 -0300

    Update Hack.java

--- Hack/src/hack/Hack.java
+++ Hack/src/hack/Hack.java
@@ -18,11 +18,10 @@ import java.util.logging.Logger;
  * @author Petrucio
  */
 public class Hack {
+
     int cont;
     Robot robot;
     Boolean ativar = false;
-    
-    
 
     public void click(int p) throws AWTException {
         robot = new Robot();
@@ -32,7 +31,7 @@ public class Hack {
             robot.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
         }
     }
-    
+
     public void alternador(int q) {
         try {
             robot = new Robot();
@@ -49,102 +48,32 @@ public class Hack {
         }
         robot.keyRelease(KeyEvent.VK_ALT);
         robot.delay(2000);
-        if (cont+1 == (q-2) && ativar == true) {
+        if (cont + 1 == (q - 2) && ativar == true) {
             cont = 0;
             ativar = false;
             System.out.println("depois");
-        } else if(cont+1==(q-2)) {
+        } else if (cont + 1 == (q - 2)) {
             ativar = true;
             System.out.println("antes");
+        } else {
+            cont++;
         }
-        
-        else cont++;
     }
-
 
     public static void main(String[] args) throws AWTException {
         Hack comando = new Hack();
-        Boolean ativo = true;
-        Boolean ativo2 = false;
         Robot robot = new Robot();
         robot.delay(10000);
-        for (int q = 0;; ) {
+        for (int q = 0;;) {
             System.out.println(robot.getPixelColor(809, 464));
-            if (robot.getPixelColor(809, 464).equals(new Color(243, 240, 204)) && ativo == true) {
+            if (robot.getPixelColor(809, 464).equals(new Color(243, 240, 204))) {
                 robot.mouseMove(809, 464);
-                comando.click(50);
-                for (;;) {
-                    if (robot.getPixelColor(975, 136).equals(new Color(255, 255, 255))) {
-                        robot.mouseMove(975, 136);
-                        comando.click(1);
-                    } else if (robot.getPixelColor(639, 138).equals(new Color(255, 255, 255))) {
-                        robot.mouseMove(975, 136);
-                        comando.click(1);
-                        break;
-                    } else if (robot.getPixelColor(819, 464).equals(new Color(255, 255, 255))){
-                        ativo2 = true;
-                        break;
-                    }else {
-                        robot.delay(50);
-                    }
-                }
-                robot.mouseMove(697, 76);
-                robot.delay(500);
-                comando.click(1);
-                for (;;) {
-                    if (robot.getPixelColor(504, 246).equals(new Color(255, 255, 255))) {
-                        robot.mouseMove(504,246);
-                        comando.click(3);
-                        if(ativo2){
-                            ativo = true;
-                            ativo2 = false;
-                        } else
-                        ativo = false;
-                        break;
-                    } else {
-                        robot.delay(100);
-                    }
-                }
-            } else if (robot.getPixelColor(809, 464).equals(new Color(243, 240, 204)) && ativo == false) {
-                robot.mouseMove(809, 464);
-                comando.click(1);
+                comando.click(51);
 
-                robot.mouseMove(697, 76);
-                robot.delay(500);
-                comando.click(1);            
-            System.out.println("Como?");
-            for (;;) {
-                if (robot.getPixelColor(504,246).equals(new Color(255,255,255))) {
-                    robot.mouseMove(504,246);
-                    comando.click(3);
-                    ativo = true;
-                    break;
-                } else {
-                    System.out.println("Erro?");
-                    robot.delay(100);
-                }
             }
 
-        }else if (robot.getPixelColor(787,454).equals(new Color(44, 44, 44))) {
-                robot.keyPress(KeyEvent.VK_ALT);
-                robot.keyPress(KeyEvent.VK_TAB);
-                robot.keyRelease(KeyEvent.VK_TAB);
-                for(int y = 0;y<(q+1);y++){
-                    robot.keyPress(KeyEvent.VK_TAB);
-                    robot.keyRelease(KeyEvent.VK_TAB);
-                    robot.delay(100);
-                }
-                robot.keyRelease(KeyEvent.VK_TAB);
-                if(q == 0) q = 0;
-            } else {
-                System.out.println("Cor nao achada");
-                robot.delay(1000);
-            }
-
+        }
     }
-
-}
-
 }
 
 /*
